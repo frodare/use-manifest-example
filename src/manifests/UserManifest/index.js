2acbec95630b9293bf8155c909deb586216198d3
Charles Howard <cdhoward@add123.com>
2020-02-03 10:54:51 -0500

    testing

--- src/manifests/UserManifest/index.js
+++ src/manifests/UserManifest/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { Manifest, useCell, DefaultManifestTable, Debug } from 'use-manifest'
 
-import useManifestState from 'use-manifest/dist/hooks/useManifestState'
-
 import users from '../../services/users'
 import def from './definition'
 
@@ -21,16 +19,14 @@ const fetchCount = async () => 2
 
 export default () => {
 
-  //const [filter, setFilter] = useState()
+  const [filter, setFilter] = useState()
 
-  const { state, setFilter } = useManifestState()
 
   return (
     <div className='App'>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
-      <button onClick={() => setFilter(true)}>test</button>
+      <button onClick={() => setFilter('test')}>test</button>
 
-      {/* <div>
+      <div>
         <button onClick={() => setFilter(true)}>Active</button>
         <button onClick={() => setFilter(false)}>Inactive</button>
         <button onClick={() => setFilter()}>All</button>
@@ -38,7 +34,7 @@ export default () => {
       <Manifest fetchRows={fetchRows} fetchCount={fetchCount} filter={{ active: filter }} definition={def}>
         <DefaultManifestTable />
         <Debug />
-      </Manifest> */}
+      </Manifest>
     </div>
   )
 }
